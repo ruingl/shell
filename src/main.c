4d66a3f2ba7784962d90bfc9f192f7cf83470a2b
ruingl <ruireogo@gmail.com>
2025-01-18 17:26:37 +0800

    feat: arguments and echo command

--- src/main.c
+++ src/main.c
@@ -14,27 +14,48 @@ void shell(char user[]) {
 
     // variables
     char command[100];
+    int args_count;
+    char *args[10];
 
     // shell
     while (1) {
         printf("$ ");
+
+        // setup cmd and args
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = '\0';
 
-        if (strcasecmp(command, "hi") == 0) {
+        char *cmd = strtok(command, " ");
+        args_count = 0;
+
+        while (cmd != NULL) {
+            args[args_count++] = cmd;
+            cmd = strtok(NULL, "");
+        }
+
+        if (args_count == 0) continue;
+
+        if (strcasecmp(args[0], "hi") == 0) {
             printf("Hello, @%s!\n", user);
-        } else if (strcasecmp(command, "exit") == 0) {
+        } else if (strcasecmp(args[0], "exit") == 0) {
             printf("Exiting...");
             break;
-        } else if (strcasecmp(command, "help") == 0) {
+        } else if (strcasecmp(args[0], "help") == 0) {
             const char *message = "Available commands:\n\n"
                                   "  1. exit\n"
                                   "  2. help\n"
                                   "  3. hi\n"
-                                  "  4. clear\n";
+                                  "  4. clear\n"
+                                  "  5. echo\n";
             printf("%s", message);
-        } else if (strcasecmp(command, "clear") == 0) {
+        } else if (strcasecmp(args[0], "clear") == 0) {
             system("clear");
+        } else if (strcasecmp(args[0], "echo") == 0) {
+            if (args_count < 2) {
+                printf("\n");
+            } else {
+                printf("%s\n", args[1]);
+            }
         } else {
             printf(
                 "error: command '%s' is not found!\n",
